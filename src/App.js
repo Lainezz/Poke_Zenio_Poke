1eababe6914c8bf3a45624de26e80cde5aec0799
Lainezz <diegolinaresmojacar@gmail.com>
2021-12-14 19:06:26 +0100

    cambios con poenodje

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
+
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [pokemon, setPokemon] = useState();
+  
+  function getId(evento){
+    console.log(evento.target.value);
+    const pokemonId = evento.target.value;
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+      .then(response => response.json())
+      .then(pokemonFromApi => {
+        setPokemon(pokemonFromApi);
+        console.log(pokemonFromApi);
+        console.log(pokemon);
+      });
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
       <div>
-        
+        <label>PokemonID</label>
+        <input type="number" onChange={getId}></input>
       </div>
+      {pokemon && (
+        <div>
+          <p>Nombre: {pokemon.name}</p>
+          <img src={pokemon.sprites.front_default}></img>
+        </div>
+      )}
     </div>
   );
 }
