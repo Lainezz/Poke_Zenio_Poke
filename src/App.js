77b87a9800643357f828cc51c15c947ffbbb5218
Lainezz <diegolinaresmojacar@gmail.com>
2021-12-14 20:06:00 +0100

    cambio con estilos del tipo

--- src/App.js
+++ src/App.js
@@ -18,22 +18,47 @@ function App() {
       });
   }
 
+  function getClassNameForType(type){
+    
+    switch(type){
+      case "grass":
+        return "green";
+      case "water":
+        return "blue";
+      case "poison":
+        return "purple";
+      case "fire":
+        return "red";
+      case "flying":
+        return "gray";
+      case "electric":
+        return "yellow";
+      default:
+        return "";
+    }
+  }
+  
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <div>
-        <label>PokemonID</label>
-        <input type="number" onChange={getId}></input>
-      </div>
-      {pokemon && (
+    <div className="container">
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
         <div>
-          <p>Nombre: {pokemon.name}</p>
-          <img src={pokemon.sprites.front_default}></img>
+          <label>PokemonID</label>
+          <input type="number" onChange={getId}></input>
         </div>
-      )}
+        {pokemon && (
+          <div>
+            <p>Nombre: {pokemon.name}</p>
+            <img src={pokemon.sprites.front_default}></img>
+            {pokemon.types.map((type)=>(
+              <p className={getClassNameForType(type.type.name)}>{type.type.name}</p>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
