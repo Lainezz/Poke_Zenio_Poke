49bfb2feb08431efeba6b2b2e794cbecd583d562
Lainezz <diegolinaresmojacar@gmail.com>
2021-12-15 14:03:50 +0100

    Formulario con id y tipo

--- src/App.js
+++ src/App.js
@@ -6,14 +6,31 @@ import { useState } from 'react';
 function App() {
   const [pokemon, setPokemon] = useState();
   
-  function getId(evento){
-    console.log(evento.target.value);
-    const pokemonId = evento.target.value;
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+  function handleForm(evento){
+    evento.preventDefault();
+    
+    console.log(evento.target.id.value);
+    
+    const pokemonId = evento.target.id.value;
+    if(pokemonId){
+      doFetch(pokemonId);
+      return;
+    }
+
+    const pokemonName = evento.target.name.value;
+    if(pokemonName){
+      doFetch(pokemonName);
+      return;
+    }
+
+    console.log("aqui");
+  }
+
+  function doFetch(valorPokemonToFetch){
+    fetch(`https://pokeapi.co/api/v2/pokemon/${valorPokemonToFetch}`)
       .then(response => response.json())
       .then(pokemonFromApi => {
         setPokemon(pokemonFromApi);
-        console.log(pokemonFromApi);
         console.log(pokemon);
       });
   }
@@ -45,12 +62,30 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        <div>
-          <label>PokemonID</label>
-          <input type="number" onChange={getId}></input>
-        </div>
+        <form onSubmit={handleForm}>
+        
+          <div className="row p-2">
+            <div className="col-md-6 col-xs-12">
+              <label>ID Pokemon</label>
+            </div>
+            <div className="col-md-6 col-xs-12">
+              <input type="number" id="id" placeholder="Seleccione ID"/>
+            </div> 
+            <div className="col-md-6 col-xs-12">
+              <label>Nombre Pokemon</label>
+            </div>
+            <div className="col-md-6 col-xs-12">
+              <input type="text" id="name" placeholder="Escriba Nombre"/>
+            </div>
+          </div>
+          <div className="d-flex justify-content-center pb-2">
+              <button className="btn btn-success" type="submit">Comprobar</button>
+              <button className="btn btn-danger" type="reset">Limpiar</button>
+          </div>
+        
+        </form>
         {pokemon && (
-          <div>
+          <div className="caja">
             <p>Nombre: {pokemon.name}</p>
             <img src={pokemon.sprites.front_default}></img>
             {pokemon.types.map((type)=>(
